f58e08c5f9c5e1f9f5be0164b8a4624302c1088e
Eichler, Marcel <marcel.eichler@outlook.com>
2022-12-09 21:29:06 +0000

    added POST and DELETE endpoints

--- backend/src/main/java/de/marcel1802/Rest/EventRest.java
+++ backend/src/main/java/de/marcel1802/Rest/EventRest.java
@@ -1,12 +1,16 @@
 package de.marcel1802.Rest;
 
+import de.marcel1802.entities.Event;
 import de.marcel1802.entities.EventDTO;
 import de.marcel1802.service.EventService;
 
 import javax.inject.Inject;
 import javax.transaction.Transactional;
+import javax.websocket.server.PathParam;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.Response;
 
@@ -27,6 +31,20 @@ public class EventRest {
 	@Path("event")
 	public Response createEvent(EventDTO event) {
 		return eventService.createEvent(event);
+	}
+
+	@Transactional
+	@PUT
+	@Path("event")
+	public Response updateEvent(Event event) {
+		return eventService.updateEvent(event);
+	}
+
+	@Transactional
+	@DELETE
+	@Path("event/{id}")
+	public Response deleteEvent(@PathParam("id") int id) {
+		return eventService.deleteEvent(id);
 	}
 
 }
--- backend/src/main/java/de/marcel1802/service/EventService.java
+++ backend/src/main/java/de/marcel1802/service/EventService.java
@@ -2,6 +2,7 @@ package de.marcel1802.service;
 
 import de.marcel1802.entities.Event;
 import de.marcel1802.entities.EventDTO;
+import de.marcel1802.entities.ResponseMessage;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.ws.rs.core.Response;
@@ -21,5 +22,21 @@ public class EventService {
 		Event newEvent = new Event(eventDTO.getName(), eventDTO.getDate());
 		newEvent.persist();
 		return Response.status(201).entity(newEvent).build();
+	}
+
+	public Response updateEvent(Event event) {
+		Event eventFromDB = Event.findById(event.getId());
+		if (eventFromDB == null) return Response.status(401).entity(new ResponseMessage("Invalid event provided.")).build();
+		eventFromDB.setDate(event.getDate());
+		eventFromDB.setName(event.getName());
+		eventFromDB.setDeleted(event.isDeleted());
+		return Response.status(200).entity(eventFromDB).build();
+	}
+
+	public Response deleteEvent(int id) {
+		Event eventFromDB = Event.findById(id);
+		if (eventFromDB == null) return Response.status(401).entity(new ResponseMessage("Invalid event provided.")).build();
+		eventFromDB.setDeleted(true);
+		return Response.status(200).entity(eventFromDB).build(); 
 	}
 }
