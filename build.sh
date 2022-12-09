8029bf020a21e90fb850b901aada9bf563bf0717
Eichler, Marcel <marcel.eichler@outlook.com>
2022-12-09 21:17:09 +0000

    build script updated

--- build.sh
+++ build.sh
@@ -2,33 +2,33 @@
 
 ROOTDIR=$(pwd)
 
-printf "\033[91m\n\n========== [1/4] Laufende Container stoppen ==========\n\n"
+printf "\033[91m\n\n========== [1/4] Laufende Container stoppen ==========\n\n\033[0m"
 docker compose down
-printf "\033[32m\nFertig."
+printf "\033[32m\nFertig.\033[0m"
 
-printf "\033[91m\n\n========== [2/4] Backend bauen ==========\n\n"
-cd ${ROOTDIR}/backend/projektverwaltung
+printf "\033[91m\n\n========== [2/4] Backend bauen ==========\n\n\033[0m"
+cd ${ROOTDIR}/backend
 ./mvnw clean package
-printf "\033[32m\nFertig."
+printf "\033[32m\nFertig.\033[0m"
 
-printf "\033[91m\n\n========== [3/4] Frontend bauen ==========\n\n"
+printf "\033[91m\n\n========== [3/4] Frontend bauen ==========\n\n\033[0m"
 cd ${ROOTDIR}/frontend
 ng build --output-hashing=none
-printf "\033[32m\nFertig."
+printf "\033[32m\nFertig.\033[0m"
 
-printf "\033[91m\n\n========== [4/4] Dateien verschieben ==========\n\n"
+printf "\033[91m\n\n========== [4/4] Dateien verschieben ==========\n\n\033[0m"
 rm -R ${ROOTDIR}/volumes/frontend/*
 rm -R ${ROOTDIR}/volumes/backend/*
 cd ${ROOTDIR}
-cp -R ./backend/projektverwaltung/target/* ./volumes/backend
-cp -R ./frontend/dist/projektverwaltung/* ./volumes/frontend
+cp -R ./backend/target/*-runner.jar ./volumes/backend/backend.jar
+cp -R ./frontend/dist/frontend/* ./volumes/frontend/
 touch ${ROOTDIR}/volumes/frontend/.gitkeep
 touch ${ROOTDIR}/volumes/backend/.gitkeep
-printf "\033[32m\nFertig."
-
+printf "\033[32m\nFertig.\033[0m"
+printf "\033[92m\n>>> Vorgang abgeschlossen. Applikation gebaut.\033[0m\n\n"
+docker compose up -d
+sleep 5
 clear
-printf "\033[92m\n>>> Vorgang abgeschlossen. Applikation gebaut."
-
 
 
 
