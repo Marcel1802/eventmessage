ee8f7921c33ae3ac1eaaec781942db50a6628bdf
Eichler, Marcel <marcel.eichler@outlook.com>
2022-12-09 20:19:51 +0000

    added test data

---
+++ backend/src/main/resources/database-example-data.sql
@@ -0,0 +1,5 @@
+INSERT INTO events (id,date,deleted,name) VALUES (1,'2022-12-09 13:51:24.000000',0,'Example event1');
+INSERT INTO events (id,date,deleted,name) VALUES (2,'2022-12-12 13:51:24.000000',0,'Example event2');
+INSERT INTO events (id,date,deleted,name) VALUES (3,'2022-12-09 13:51:24.000000',0,'Example event3');
+INSERT INTO events (id,date,deleted,name) VALUES (4,'2023-01-12 13:51:24.000000',1,'Example event4');
+INSERT INTO events (id,date,deleted,name) VALUES (5,'2023-01-12 20:00:00.000000',0,'Example event5');
