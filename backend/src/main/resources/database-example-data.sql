018b638e35ff01c1aabb47dec102909d1cfee278
Eichler, Marcel <marcel.eichler@outlook.com>
2022-12-10 13:03:36 +0000

    more example data

--- backend/src/main/resources/database-example-data.sql
+++ backend/src/main/resources/database-example-data.sql
@@ -1,5 +1,10 @@
 INSERT INTO events (id,date,deleted,name) VALUES (1,'2022-12-09 13:51:24.000000',0,'Example event1');
-INSERT INTO events (id,date,deleted,name) VALUES (2,'2022-12-12 13:51:24.000000',0,'Example event2');
+INSERT INTO events (id,date,deleted,name) VALUES (2,'2022-12-12 13:51:24.000000',1,'Example event2');
 INSERT INTO events (id,date,deleted,name) VALUES (3,'2022-12-09 13:51:24.000000',0,'Example event3');
 INSERT INTO events (id,date,deleted,name) VALUES (4,'2023-01-12 13:51:24.000000',1,'Example event4');
 INSERT INTO events (id,date,deleted,name) VALUES (5,'2023-01-12 20:00:00.000000',0,'Example event5');
+INSERT INTO events (id,date,deleted,name) VALUES (6,'2023-12-09 13:51:24.000000',0,'Example event6');
+INSERT INTO events (id,date,deleted,name) VALUES (7,'2023-12-12 13:51:24.000000',1,'Example event7');
+INSERT INTO events (id,date,deleted,name) VALUES (8,'2023-12-15 13:51:24.000000',0,'Example event8');
+INSERT INTO events (id,date,deleted,name) VALUES (9,'2023-01-12 13:51:24.000000',1,'Example event9');
+INSERT INTO events (id,date,deleted,name) VALUES (10,'2024-01-12 20:00:00.000000',0,'Example event10');
